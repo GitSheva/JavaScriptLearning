e872cb2fb4674d5b62e944ff27780de7d1356c51
Your Name <aryabko@gmail.com>
2018-08-18 08:17:13 -0700

    working with functions

--- functions.js
+++ functions.js
@@ -1,8 +1,28 @@
-//Functions
-'use strict';
+/*
+ * Functions always return a value. In JavaScript, if no return value is specified, the function will return undefined.
+ * Functions are objects.
+ * When using function Declarations, the function definition is hoisted, thus allowing the function to be used before it is defined.
+ * A Function Expressions defines a named or anonymous function.  Function Expressions are not hoisted, and therefore cannot be used before they are defined.
+ */
 
-let f = function() {
-    console.log(this);
-};
+//Function Declarations
+console.log('Function Declarations');
 
-f();
+//definition is hoisted and awailable
+console.log(square(5));
+
+function square(n) {
+
+    return n * n;
+}
+
+//Function Expressions
+console.log('Function Expressions');
+
+//function is not hoisted, so cannot be used before definition, this line will throw error "sum is not defined"
+console.log(sum(5, 2));
+
+let sum = function(a, b) {
+
+    return a + b;
+}
