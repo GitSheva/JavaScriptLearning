0f47c3e705876455d48c2d2ffd6a314989769479
Your Name <aryabko@gmail.com>
2018-08-18 08:45:01 -0700

    finished with functions

--- functions.js
+++ functions.js
@@ -1,8 +1,10 @@
 /*
  * Functions always return a value. In JavaScript, if no return value is specified, the function will return undefined.
- * Functions are objects.
+ * Functions are objects. In JavaScript, anything that is not a primitive type ( undefined, null,boolean, number, or string) is an object. 
  * When using function Declarations, the function definition is hoisted, thus allowing the function to be used before it is defined.
  * A Function Expressions defines a named or anonymous function.  Function Expressions are not hoisted, and therefore cannot be used before they are defined.
+ * Arrow Function Expression, they do not create their own "this" value.
+ * Values can be passed into functions and used within the function. The name of the value is called a parameter. The actual value itself is called an argument.
  */
 
 //Function Declarations
@@ -20,9 +22,27 @@ function square(n) {
 console.log('Function Expressions');
 
 //function is not hoisted, so cannot be used before definition, this line will throw error "sum is not defined"
-console.log(sum(5, 2));
+//uncomment to test
+console.log('Uncomment to test!');
+//console.log(sum(5, 2));
 
 let sum = function(a, b) {
 
     return a + b;
 }
+
+//Arrow Function Expression
+console.log('Arrow Function Expression');
+
+let showMessage = () => {
+
+    console.log('Hello');
+}
+
+showMessage();
+
+//Self executed arrow function expression
+(() => {
+
+    console.log('Hello Self Executed');
+})()
