77a0688f3ba90135769ae4c42f367b63f28d68c9
Your Name <aryabko@gmail.com>
2018-08-18 22:42:12 -0700

    adit..

--- this.js
+++ this.js
@@ -2,6 +2,11 @@
  *Global scope
  *Function scope
  *Object scope
+
+*Methods can reference the object as this. 
+*The value of this is defined at run-time.
+*When a function is declared, it may use this, but that this has no value until the function is called. That function can be copied between objects.
+*When a function is called in the “method” syntax: object.method(), the value of this during the call is object.
  */
 
 //Global scope, this = Window
