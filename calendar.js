03af7e98bc21aba6ad598d79fcd5a9bc9ccbd6b3
GitSheva <aryabko@gmail.com>
2018-11-13 16:48:54 -0800

    upd

--- calendar.js
+++ calendar.js
@@ -1,4 +1,10 @@
 function start(){
-    alert('haahah');
-    document.getElementById("year").value="2017";
+    
+    let now = new Date();
+    let currentYear = now.getFullYear();
+    let currentMonth = now.getMonth() + 1;
+    //let day = now.getDate();
+    
+    document.getElementById("year").value = currentYear;
+    document.getElementById("month").value = currentMonth;
 }
